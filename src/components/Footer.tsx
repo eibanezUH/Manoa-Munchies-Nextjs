776c45bdc011c45213587fe6cffbed1785bc4fbf
shaylonmizukami <smizukami15@gmail.com>
2025-04-15 16:18:00 -1000

    Merged landing page
    Added css files
    Added image
    edited page.tsx in src/app

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,32 @@
-import { Col, Container } from 'react-bootstrap';
+// import { Col, Container } from 'react-bootstrap';
+
+// /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
+// const Footer = () => (
+//   <footer className="mt-auto py-3 bg-light">
+//     <Container>
+//       <Col className="text-center">
+//         Department of Information and Computer Sciences
+//         <br />
+//         University of Hawaii
+//         <br />
+//         Honolulu, HI 96822
+//         <br />
+//         <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
+//       </Col>
+//     </Container>
+//   </footer>
+// );
+
+// export default Footer;
+
+import { Container } from 'react-bootstrap';
+import '../app/footer.css';
 
 /** The Footer appears at the bottom of every page. Rendered by the App Layout component. */
 const Footer = () => (
   <footer className="mt-auto py-3 bg-light">
     <Container>
-      <Col className="text-center">
-        Department of Information and Computer Sciences
-        <br />
-        University of Hawaii
-        <br />
-        Honolulu, HI 96822
-        <br />
-        <a href="http://ics-software-engineering.github.io/nextjs-application-template">Template Home Page</a>
-      </Col>
+      {/* Intentionally left blank. Add links or info here if desired. */}
     </Container>
   </footer>
 );
